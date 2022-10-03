70c0b274e358ba41670d46e3e5fdb5527e1d3933
youngjinc <cyjin6789@gmail.com>
2022-10-03 18:20:33 +0900

    [add] 토스트 띄우기 확장함수 추가

---
+++ app/src/main/java/org/sopt/sample/util/extensions/ContextExt.kt
@@ -0,0 +1,13 @@
+package org.sopt.sample.util.extensions
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String, isShort: Boolean = true) {
+    val duration = if (isShort) {
+        Toast.LENGTH_SHORT
+    } else {
+        Toast.LENGTH_LONG
+    }
+    Toast.makeText(this, message, duration).show()
+}
