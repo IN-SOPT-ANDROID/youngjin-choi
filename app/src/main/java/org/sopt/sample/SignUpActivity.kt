0a13866b52e3f79c99be48aba678633f99b5f817
youngjinc <cyjin6789@gmail.com>
2022-10-03 13:27:37 +0900

    [feat] 회원가입 화면 구현

---
+++ app/src/main/java/org/sopt/sample/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
