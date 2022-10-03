0f3bb90cebc96b5db6afd83b4b05b812aaa2e93e
youngjinc <cyjin6789@gmail.com>
2022-10-03 16:13:06 +0900

    [feat] 로그인 화면에서 회원가입 버튼 클릭 시 회원가입 화면으로 전환 처리

--- app/src/main/java/org/sopt/sample/SignInActivity.kt
+++ app/src/main/java/org/sopt/sample/SignInActivity.kt
@@ -1,5 +1,6 @@
 package org.sopt.sample
 
+import android.content.Intent
 import android.os.Bundle
 import org.sopt.sample.base.BaseActivity
 import org.sopt.sample.databinding.ActivitySignInBinding
@@ -7,5 +8,13 @@ import org.sopt.sample.databinding.ActivitySignInBinding
 class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.btnSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
