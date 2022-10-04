3b5a2f1bd2dfc7b2fe58a7d5e4066e745f62824a
youngjinc <cyjin6789@gmail.com>
2022-10-04 17:42:16 +0900

    [del] 사용되지 않는 변수 제거

--- app/src/main/java/org/sopt/sample/application/InSoptApplication.kt
+++ app/src/main/java/org/sopt/sample/application/InSoptApplication.kt
@@ -4,15 +4,10 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import org.sopt.sample.BuildConfig
 import org.sopt.sample.util.InSoptDebugTree
-import org.sopt.sample.util.InSoptSharedPreference
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
 class InSoptApplication : Application() {
-    @Inject
-    lateinit var inSoptSharedPreference: InSoptSharedPreference
-
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
