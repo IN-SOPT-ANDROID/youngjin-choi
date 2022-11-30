b5c542b38913169db7d19c09b7b073e46af671ed
youngjinc <cyjin6789@gmail.com>
2022-11-30 19:43:55 +0900

    [refactor] 스낵바 확장 함수 코드 정리

--- app/src/main/java/org/sopt/sample/util/extensions/ViewExt.kt
+++ app/src/main/java/org/sopt/sample/util/extensions/ViewExt.kt
@@ -4,10 +4,6 @@ import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showSnackbar(message: String, isShort: Boolean = true) {
-    val duration = if (isShort) {
-        Snackbar.LENGTH_SHORT
-    } else {
-        Snackbar.LENGTH_LONG
-    }
+    val duration = if (isShort) Snackbar.LENGTH_SHORT else Snackbar.LENGTH_LONG
     Snackbar.make(this, message, duration).show()
 }
