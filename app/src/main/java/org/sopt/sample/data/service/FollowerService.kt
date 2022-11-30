109cf6d1dfa28a26c532f638d332266d3e64902b
youngjinc <cyjin6789@gmail.com>
2022-11-30 13:33:07 +0900

    [chore] 팔로워 조회 요청 url 변경

--- app/src/main/java/org/sopt/sample/data/service/FollowerService.kt
+++ app/src/main/java/org/sopt/sample/data/service/FollowerService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FollowerService {
-    @GET("/api/users?page=")
+    @GET("/api/users")
     suspend fun fetchFollowerList(
         @Query("page") site: Int = 2,
     ): ResponseFollower
